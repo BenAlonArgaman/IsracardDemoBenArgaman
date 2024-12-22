4d7e550d7953101670a0e32dd759fc9ba505cf46
BenAlonArgaman <baappdevs@gmail.com>
2024-12-22 14:48:11 +0200

    Update _layout.tsx

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,9 +1,21 @@
 import { SortMenu } from "@/components/SortMenu";
 import { router, Stack } from "expo-router";
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { Colors } from "@/constants/Colors";
 
 const Layout = () => {
   return (
-    <Stack screenOptions={{ headerLargeTitle: true }}>
+    <Stack
+      screenOptions={{
+        headerLargeTitle: true,
+        contentStyle: {
+          backgroundColor:
+            useColorScheme() === "dark"
+              ? Colors.dark.background
+              : Colors.light.background,
+        },
+      }}
+    >
       <Stack.Screen
         name="index"
         options={{
